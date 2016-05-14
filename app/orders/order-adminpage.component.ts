4ae0419c80bb4c1f89e6eda26a886a765570aaa3
Lucidity <dom.dj@outlook.com>
2016-05-14 13:30:18 +1000

    Orders service/component + package updates
    Start of orders service and orders component. Added typescript and other
    packages

---
+++ app/orders/order-adminpage.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+import { IOrders } from './orders';
+import { OrdersService } from './orders.service';
+
+@Component({
+    templateUrl: 'app/orders/order-adminpage.component.html',
+    directives: [ROUTER_DIRECTIVES]
+})
+
+export class OrdersComponent implements OnInit {
+    
+    orders: IOrders[];
+    errorMessage: string;
+    
+    constructor(private _ordersService: OrdersService) {
+        
+    }
+    
+    ngOnInit(): void {
+        this._ordersService.getOrders()
+        .subscribe(
+            orders => this.orders = orders,
+            error => this.errorMessage = <any>error);
+    }
+}
---
+++ app/orders/orders.ts
@@ -0,0 +1,13 @@
+export interface IOrders {
+    custId: number;
+    custName: string;
+    productName: string;
+    orderSize: string;
+    progress: string;
+    orderType: string;
+    custPortrait: string;
+    orderDate: string;
+    orderTime: string;
+    deliveryMethod: string;
+    totalCost: number;
+}
